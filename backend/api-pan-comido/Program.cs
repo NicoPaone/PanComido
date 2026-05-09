2e774b3966bf5b438381a1bb5ad1bcf2b26b8ad7
Urielito1031 <urielestebanyurquina@gmail.com>
2026-05-09 00:33:10 -0300

    Configurando CORS

--- backend/api-pan-comido/Program.cs
+++ backend/api-pan-comido/Program.cs
@@ -7,6 +7,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var allowedOrigins = builder.Configuration.GetSection("CorsSettings:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>();
+
+
+
+builder.Services.AddCors(options =>
+{
+   options.AddPolicy("ProduccionCors", policy =>
+   {
+      policy.WithOrigins(allowedOrigins)
+      .AllowAnyMethod()
+      .AllowAnyHeader();
+   });
+
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ProduccionCors");
 
 app.UseAuthorization();
 
