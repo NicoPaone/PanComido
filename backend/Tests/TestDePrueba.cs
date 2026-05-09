4b6bd14805b1ca86b86f5832fa7b9de4ec98017e
Urielito1031 <urielestebanyurquina@gmail.com>
2026-05-08 23:39:07 -0300

    web api La verdadera

--- backend/Tests/TestDePrueba.cs
+++ backend/Tests/TestDePrueba.cs
@@ -2,18 +2,21 @@ namespace Tests
 {
    public class TestDePrueba
    {
-      [Fact]
-      public void Test1()
-      {
-         Assert.True(true);
+     
+         [Fact]
+         public void Test1()
+         {
+            Assert.True(true);
 
-      }
+         }
 
-      [Fact]
-      public void Otro_test_que_da_true()
-      {
-         Assert.True(true);
+         [Fact]
+         public void Otro_test_que_da_true()
+         {
+            Assert.True(true);
 
-      }
+         }
+
+      
    }
 }
