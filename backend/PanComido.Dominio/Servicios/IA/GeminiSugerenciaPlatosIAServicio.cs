8edfa4a3c1a60f2d99d8b6d700f086ccb8dff16e
Nicolas Paone <nicolasarielpaone@gmail.com>
2026-06-01 19:46:13 -0300

    Correcion para poder traerme develop

---
+++ backend/PanComido.Dominio/Servicios/IA/GeminiSugerenciaPlatosIAServicio.cs
@@ -0,0 +1,19 @@
+﻿using PanComido.Dominio.Entidades;
+using PanComido.Dominio.Entidades.IA;
+using PanComido.Dominio.Interfaces.Servicios.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanComido.Dominio.Servicios.IA
+{
+    public class GeminiSugerenciaPlatosIAServicio : ISugerenciaPlatosIAServicio
+    {
+        public Task<SugerenciaIA> GenerarSugerenciasAsync(List<Insumo> insumosDisponibles, Dictionary<int, List<Lote>> vencimientosProximos, int cantidadPlatos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
