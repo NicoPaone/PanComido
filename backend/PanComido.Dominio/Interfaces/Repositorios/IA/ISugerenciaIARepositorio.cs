c5196d921511e6f4745806e912cee3606792f04d
Nicolas Paone <nicolasarielpaone@gmail.com>
2026-06-01 05:32:46 -0300

    Preparacion completa para hacer la integracion con la IA

---
+++ backend/PanComido.Dominio/Interfaces/Repositorios/IA/ISugerenciaIARepositorio.cs
@@ -0,0 +1,16 @@
+﻿using PanComido.Dominio.Entidades.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanComido.Dominio.Interfaces.Repositorios.IA
+{
+    public interface ISugerenciaIARepositorio
+    {
+        Task<SugerenciaIA?> ObtenerSugerenciaIAAsync(int restauranteId);
+
+        Task GuardarSugerenciaIAAsync(int restauranteId, SugerenciaIA sugerenciaIA);
+    }
+}
---
+++ backend/PanComido.Dominio/Interfaces/Servicios/IA/ISugerenciaPlatosIAServicio.cs
@@ -0,0 +1,17 @@
+﻿using PanComido.Dominio.Entidades;
+using PanComido.Dominio.Entidades.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanComido.Dominio.Interfaces.Servicios.IA
+{
+    public interface ISugerenciaPlatosIAServicio
+    {
+        Task<SugerenciaIA> GenerarSugerenciasAsync(List<Insumo> insumosDisponibles,
+                                                    Dictionary<int, List<Lote>> vencimientosProximos,
+                                                    int cantidadPlatos);
+    }
+}
