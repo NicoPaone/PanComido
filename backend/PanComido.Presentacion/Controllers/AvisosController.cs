7838894893a1165967007f2b7f6dc64b492f366b
Nicolas Paone <nicolasarielpaone@gmail.com>
2026-06-01 04:16:14 -0300

    Listado de insumos con vencimiento proximo y poco stock terminados, comienzo de las bases para la integracion de sugerencias con IA

---
+++ backend/PanComido.Presentacion/Controllers/AvisosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PanComido.Dominio.CasosDeUso.AvisosCasosDeUso;
+using PanComido.Presentacion.DTOs.Avisos;
+using PanComido.Presentacion.Mappers;
+using PanComido.Presentacion.SesionMock;
+
+namespace PanComido.Presentacion.Controllers
+{
+    [ApiController]
+    [Route("[avisos]")]
+    public class AvisosController : ControllerBase
+    {
+        private readonly ListarInsumosConStockCriticoCasoDeUso _listarInsumosConStockCriticoCasoDeUso;
+        private readonly ListarInsumosConVencimientoProximoCasoDeUso _listarInsumosConVencimientoProximoCasoDeUso;
+        private readonly InsumoMapper _insumoMapper;
+        private readonly LoteMapper _loteMapper;
+
+        public AvisosController(ListarInsumosConStockCriticoCasoDeUso listarInsumosConStockCriticoCasoDeUso,
+                                ListarInsumosConVencimientoProximoCasoDeUso listarInsumosConVencimientoProximoCasoDeUso,
+                                InsumoMapper insumoMapper,
+                                LoteMapper loteMapper)
+        {
+            _listarInsumosConStockCriticoCasoDeUso = listarInsumosConStockCriticoCasoDeUso;
+            _listarInsumosConVencimientoProximoCasoDeUso = listarInsumosConVencimientoProximoCasoDeUso;
+            _insumoMapper = insumoMapper;
+            _loteMapper = loteMapper;
+        }
+
+        [HttpGet("/avisos")]
+        public async Task<ActionResult<AvisosResponseDto>> obtener()
+        {
+            var restauranteId = HttpContext.ObtenerRestauranteId();
+            var insumosConStockCritico = await _listarInsumosConStockCriticoCasoDeUso.EjecutarAsync(restauranteId);
+            var insumosConVencimientoProximo = await _listarInsumosConVencimientoProximoCasoDeUso.EjecutarAsync(restauranteId);
+
+            var response = new AvisosResponseDto
+            {
+                InsumosConStockCritico = _insumoMapper.aListaDto(insumosConStockCritico),
+                InsumosConVencimientoProximo = _loteMapper.aDiccionarioDto(insumosConVencimientoProximo)
+            };
+
+            return Ok(response);
+        }
+    }
+}
--- backend/PanComido.Presentacion/Program.cs
+++ backend/PanComido.Presentacion/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PanComido.Dominio.CasosDeUso.AvisosCasosDeUso;
 using PanComido.Dominio.CasosDeUso.BodegaCasosDeUso;
 using PanComido.Dominio.CasosDeUso.ComandaCasosDeUso;
 using PanComido.Dominio.CasosDeUso.InsumoCasosDeUso;
@@ -60,7 +61,7 @@ builder.Services.AddScoped<CategoriaInsumoMapper>();
 builder.Services.AddScoped<InsumoConsugerenciaMapper>();
 builder.Services.AddScoped<UnidadMedidaMapper>();
 builder.Services.AddScoped<LoteRecepcionMapper>();
-builder.Services.AddScoped<LlamadoMapper>();
+builder.Services.AddScoped<LoteMapper>();
 
 // Repositorios
 builder.Services.AddScoped<IInsumoRepositorio, InsumoRepositorio>();
@@ -93,9 +94,8 @@ builder.Services.AddScoped<OcuparMesaCasoDeUso>();
 builder.Services.AddScoped<EnviarPedidoProveedorCasoDeUso>();
 builder.Services.AddScoped<GenerarSugerenciasRecepcionCasoDeUso>();
 builder.Services.AddScoped<RecibirPedidoProveedorCasoDeUso>();
-builder.Services.AddScoped<LlamarMozoCasoDeUso>();
-builder.Services.AddScoped<ListarLlamadosPendientesCasoDeUso>();
-builder.Services.AddScoped<ResolverLlamadoCasoDeUso>();
+builder.Services.AddScoped<ListarInsumosConStockCriticoCasoDeUso>();
+builder.Services.AddScoped<ListarInsumosConVencimientoProximoCasoDeUso>();
 
 // Servicios
 builder.Services.AddScoped<IEstadoStockInsumoServicio, EstadoStockInsumoServicio>();
