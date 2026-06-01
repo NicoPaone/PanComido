a0bbc27b284606a0229289b1b351217c9eabd204
Urielito1031 <urielestebanyurquina@gmail.com>
2026-06-01 02:32:23 -0300

    flujo de lista de mesas hecho

---
+++ backend/PanComido.Dominio/CasosDeUso/MesaCasosDeUso/ListarMesasCasoDeUso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PanComido.Dominio.Entidades;
+using PanComido.Dominio.Interfaces.Repositorios;
+
+namespace PanComido.Dominio.CasosDeUso.MesaCasosDeUso
+{
+   public class ListarMesasCasoDeUso
+   {
+      private readonly IMesaRepositorio _mesaRepositorio;
+
+      public ListarMesasCasoDeUso(IMesaRepositorio mesaRepositorio)
+      {
+         _mesaRepositorio = mesaRepositorio;
+      }
+      public async Task<List<MesaConPosiciones>> EjecutarAsync(int restauranteId)
+      {
+         return await _mesaRepositorio.ObtenerTodasAsync(restauranteId);
+      }
+   }
+}
