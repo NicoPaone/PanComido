72f6490e25a290002c5c82426787d60a540f9e31
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2026-06-02 05:05:10 -0300

    agregado caso de uso para marcar un item de la comanda como entregado, agregado de validaciones

--- backend/PanComido.Presentacion/Controllers/ComandaController.cs
+++ backend/PanComido.Presentacion/Controllers/ComandaController.cs
@@ -5,43 +5,51 @@ using PanComido.Presentacion.Mappers;
 
 namespace PanComido.Presentacion.Controllers
 {
-   [Route("comanda")]
-   [ApiController]
-   public class ComandaController : ControllerBase
-   {
-      private readonly ListarComandaActivaCocinaCasoDeUso _listarComandasActivasCocinaCasoDeUso;
-      private readonly ModificarEstadoComandaCasoDeUso _modificarEstadoComandaCasoDeUso;
-      private readonly ComandaMapper _mapper;
-      public ComandaController(ListarComandaActivaCocinaCasoDeUso listarComandaActivasCasoDeUso, ModificarEstadoComandaCasoDeUso modificar, ComandaMapper mapper)
-      {
-         _listarComandasActivasCocinaCasoDeUso = listarComandaActivasCasoDeUso;
-         _modificarEstadoComandaCasoDeUso = modificar;
-         _mapper = mapper;
-      
-      }
-      [HttpGet("activas")]
-      public async Task<ActionResult<List<ComandaResponseDto>>> ObtenerComandasActivas()
-      {
-         int restauranteId = 1;
-         var comandas = await _listarComandasActivasCocinaCasoDeUso.Ejecutar(restauranteId);
-         var comandasDto = _mapper.ComandaResponseDtoList(comandas);
-         return Ok(comandasDto);
+    [Route("comanda")]
+    [ApiController]
+    public class ComandaController : ControllerBase
+    {
+        private readonly ListarComandaActivaCocinaCasoDeUso _listarComandasActivasCocinaCasoDeUso;
+        private readonly ModificarEstadoComandaCasoDeUso _modificarEstadoComandaCasoDeUso;
+        private readonly MarcarItemEntregadoCasoDeUso _marcarItemEntregadoCasoDeUso;
+        private readonly ComandaMapper _mapper;
 
-      }
-      [HttpPut("activas/{mesaId}/{estadoId}")]
-      public async Task<ActionResult<ComandaResponseDto>> ModificarEstadoDeComanda(int mesaId, int estadoId)
-      {
-         var comanda = await _modificarEstadoComandaCasoDeUso.EjecutarAsync(mesaId, estadoId);
-         var comandaDto = _mapper.ComandaResponseDto(comanda);
-         return Ok(comandaDto);
-      }
+        public ComandaController(
+            ListarComandaActivaCocinaCasoDeUso listarComandaActivasCasoDeUso,
+            ModificarEstadoComandaCasoDeUso modificar,
+            MarcarItemEntregadoCasoDeUso marcarItemEntregadoCasoDeUso,
+            ComandaMapper mapper)
+        {
+            _listarComandasActivasCocinaCasoDeUso = listarComandaActivasCasoDeUso;
+            _modificarEstadoComandaCasoDeUso = modificar;
+            _marcarItemEntregadoCasoDeUso = marcarItemEntregadoCasoDeUso;
+            _mapper = mapper;
 
+        }
 
-     
+        [HttpGet("activas")]
+        public async Task<ActionResult<List<ComandaResponseDto>>> ObtenerComandasActivas()
+        {
+            int restauranteId = 1;
+            var comandas = await _listarComandasActivasCocinaCasoDeUso.Ejecutar(restauranteId);
+            var comandasDto = _mapper.ComandaResponseDtoList(comandas);
+            return Ok(comandasDto);
 
-      
+        }
+        [HttpPut("activas/{mesaId}/{estadoId}")]
+        public async Task<ActionResult<ComandaResponseDto>> ModificarEstadoDeComanda(int mesaId, int estadoId)
+        {
+            var comanda = await _modificarEstadoComandaCasoDeUso.EjecutarAsync(mesaId, estadoId);
+            var comandaDto = _mapper.ComandaResponseDto(comanda);
+            return Ok(comandaDto);
+        }
 
-   }
-
-    
+        [HttpPut("{comandaId}/item/{articuloComandaId}/entregar")]
+        public async Task<ActionResult<ComandaResponseDto>> MarcarItemComandaEntregado(int comandaId, int articuloComandaId)
+        {
+            var comanda = await _marcarItemEntregadoCasoDeUso.EjecutarAsync(comandaId, articuloComandaId);
+            var comandaDto = _mapper.ComandaResponseDto(comanda);
+            return Ok(comandaDto);
+        } 
+    }
 }
--- backend/PanComido.Presentacion/Program.cs
+++ backend/PanComido.Presentacion/Program.cs
@@ -108,7 +108,15 @@ builder.Services.AddScoped<EnviarPedidoProveedorCasoDeUso>();
 builder.Services.AddScoped<GenerarSugerenciasRecepcionCasoDeUso>();
 builder.Services.AddScoped<RecibirPedidoProveedorCasoDeUso>();
 builder.Services.AddScoped<ListarMesasCasoDeUso>();
-
+builder.Services.AddScoped<ObtenerDatosParaFormularioCrearPlato>();
+builder.Services.AddScoped<ObtenerCartaCasoDeUso>();
+builder.Services.AddScoped<ListarInsumosConStockCriticoCasoDeUso>();
+builder.Services.AddScoped<ListarInsumosConVencimientoProximoCasoDeUso>();
+builder.Services.AddScoped<LlamarMozoCasoDeUso>();
+builder.Services.AddScoped<ListarLlamadosPendientesCasoDeUso>();
+builder.Services.AddScoped<ResolverLlamadoCasoDeUso>();
+builder.Services.AddScoped<ListarComandasActivasMozoCasoDeUso>();
+builder.Services.AddScoped<MarcarItemEntregadoCasoDeUso>();
 
 // Servicios
 builder.Services.AddScoped<IEstadoStockInsumoServicio, EstadoStockInsumoServicio>();
