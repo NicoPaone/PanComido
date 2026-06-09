48b69d0fbe8d4dbfd4c9225b1bc04d38e321b735
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2026-06-09 03:07:48 -0300

    empezada la configuracion del local, empezados la eleccion de metodos de pago

---
+++ backend/PanComido.Infraestructura/Persistencia/Mappers/MetodoDePagoEntityMapper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DOM = PanComido.Dominio.Entidades;
+using EF = PanComido.Infraestructura.Persistencia.Entidades;
+
+namespace PanComido.Infraestructura.Persistencia.Mappers
+{
+    public class MetodoDePagoEntityMapper
+    {
+        public DOM.MetodoDePago paraDominio(EF.MetodoDePago efMetodoDePago, EF.MetodoDePagoRestaurante efRelacion)
+        {
+            return new DOM.MetodoDePago
+            {
+               Id = efMetodoDePago.Id,
+               Descripcion = efMetodoDePago.Descripcion,
+               Habilitado = efRelacion.Habilitado
+            };
+        }
+
+        public EF.MetodoDePagoRestaurante paraActualizarEntidad(DOM.MetodoDePago metodoDePago)
+        {
+            return new EF.MetodoDePagoRestaurante
+            {
+                Habilitado = metodoDePago.Habilitado
+            };
+        }
+    }
+}
