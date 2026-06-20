697e734168b5c386fc01a7d66cb9cca45960552c
Urielito1031 <urielestebanyurquina@gmail.com>
2026-06-20 16:59:19 -0300

    agregando controlador de restaurante para obtener configuracion par comensal

---
+++ backend/PanComido.Presentacion/Controllers/RestauranteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PanComido.Dominio.CasosDeUso.ConfiguracionCasoDeUso;
+using PanComido.Presentacion.DTOs.Restaurante;
+using PanComido.Presentacion.Mappers;
+
+namespace PanComido.Presentacion.Controllers
+{
+   [Route("restaurante")]
+   [ApiController]
+   public class RestauranteController : ControllerBase
+   {
+      private readonly ObtenerDatosDelLocalCasoDeUso _datosLocal;
+      private readonly RestauranteMapper _mapper;
+
+      public  RestauranteController(ObtenerDatosDelLocalCasoDeUso datosLocal, RestauranteMapper mapper)
+      {
+         _datosLocal = datosLocal;
+         _mapper = mapper;
+      }
+
+      [HttpGet("{restauranteId:int}/configuracion-visual")]
+      public async Task<ActionResult<RestauranteResponseDto>>ObtenerConfiguracionVisual(int restauranteId)
+      {
+         var restaurante =await _datosLocal.EjecutarAsync(restauranteId);
+
+         if (restaurante is null) return NotFound();
+
+         var dto = _mapper.aDto(restaurante);
+         return Ok(dto);
+
+
+      }
+
+
+   }
+}
